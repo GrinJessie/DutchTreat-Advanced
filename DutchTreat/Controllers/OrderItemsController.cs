3df0cb3365afac0b7291866defab198796d58a8f
Lijing Wang <lijing@ualberta.ca>
2021-03-19 11:21:14 -0600

    added associated controller of OrderItems as a sub controller of Orders, designed the URLs to represent the relationship between entities

---
+++ DutchTreat/Controllers/OrderItemsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DutchTreat.Data.Entities;
+using DutchTreatAdvanced.Data;
+using DutchTreatAdvanced.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreatAdvanced.Controllers
+{
+    // sub controllers of orders
+    [Route("/api/orders/{orderId}/items")]
+    public class OrderItemsController : Controller
+    {
+        private readonly IDutchRepository _dutchRepository;
+        private readonly ILogger<OrderItemsController> _logger;
+        private readonly IMapper _mapper;
+
+        public OrderItemsController(IDutchRepository dutchRepository, ILogger<OrderItemsController> logger, IMapper mapper)
+        {
+            _dutchRepository = dutchRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int orderId)
+        {
+            var order = _dutchRepository.GetOrderById(orderId);
+
+            if (order != null ) return Ok(_mapper.Map<IEnumerable<OrderItem>, IEnumerable<OrderItemViewModel>>(order.Items));
+
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int orderId, int id)
+        {
+            var order = _dutchRepository.GetOrderById(orderId);
+
+            if (order != null)
+            {
+                var item = order.Items.FirstOrDefault(x => x.Id == id);
+
+                return Ok(_mapper.Map<OrderItem, OrderItemViewModel>(item));
+            }
+
+            return NotFound();
+        }
+
+    }
+}
